2241b699d6b478b20bf2f10dcfcbd57d38d0b06e
antoinebidaud <antoine.bidaud@yahoo.fr>
2020-10-15 15:15:43 +0200

    Ajout de la récupération des comics d'un caractère

--- src/components/CharacterItem.vue
+++ src/components/CharacterItem.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
         <h2>{{ info.data.results[0].name }}</h2>
+        <div v-if="info.data.results[0].description">
+        <p>{{ info.data.results[0].description }}</p>
+        </div>
+        <div v-else>il n'y a pas  de description</div>
+        <p>{{ infoComics.data.results }}</p>
         
+        <!-- <div v-for="comics in infosComics" :key="">
+        </div> -->
     </div>
 </template>
 
@@ -14,21 +21,30 @@ export default {
         return{
             info: null,
             loading: true,
-            errored: false
+            errored: false,
+            infoComics: null
         }
     },
     created () {
         
     },
     async mounted () {
-
+        const idCharacter =  this.$route.params.id ;
         try {
-            const idCharacter =  this.$route.params.id ;
+            
             const {data} = await axios.get("https://gateway.marvel.com:443/v1/public/characters/"+idCharacter+"?apikey=55dd7a6256658f33a00034a161f9c8f7&ts=1&hash=8e821d4269b2d7f35e61d11ecd39ff92")
             this.info = data
         } catch (error) {
             this.errored = true;
         }
+        try {
+            const {data} = await axios
+            .get("https://gateway.marvel.com:443/v1/public/characters/"+idCharacter+"/comics?limit=10&apikey=55dd7a6256658f33a00034a161f9c8f7&ts=1&hash=8e821d4269b2d7f35e61d11ecd39ff92")
+            this.infoComics = data
+        } catch (error) {
+            this.errored = true;
+        }
+
         this.loading = false;
     },
     methods: {
