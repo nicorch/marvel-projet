0c7c8a23f2f4b05db3a01303516fcc1762ecd8c6
antoinebidaud <antoine.bidaud@yahoo.fr>
2020-10-15 13:53:09 +0200

    ajout d'axios et liste des characters

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,3 +1,59 @@
 <template>
-    <h2>char list</h2>
+  <div>
+    <h1>char list</h1>
+  <section v-if="errored">
+    <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+  </section>
+
+  <section v-else>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+    <table class="table-auto">
+      <thead>
+        <tr>
+          <th class="px-4 py-2">Nom</th>
+          <th class="px-4 py-2">Description</th>
+          <th class="px-4 py-2">Image</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="character in info.data.results" :key="character.id">
+          <td class="border px-4 py-2">{{ character.name }}</td>
+          <td class="border px-4 py-2">{{ character.description }}</td>
+        </tr>
+      </tbody> 
+    </table>
+  </div>
+  </section>
+</div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'CharactersList',
+  data () {
+    return {
+      info : null,
+      loading: true,
+      errored: false
+    }
+  },
+  async mounted() {
+    try {
+
+      const response = await axios.get("https://gateway.marvel.com:443/v1/public/characters?apikey=55dd7a6256658f33a00034a161f9c8f7&ts=1&hash=8e821d4269b2d7f35e61d11ecd39ff92")
+      this.info = response.data;
+    } catch (error) {
+      this.errored = true;
+    }
+    this.loading = false
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
