fce7bd829e7c5280a0618ad75506bbf0e285d04f
NicolasRoche <nicolas8roche@gmail.com>
2020-10-15 14:18:06 +0200

    ComicCard static component

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="comic-item-card">
+      <a :href="url_item">
+        <div class="card text-left">
+            <img class="card-img-top"  :src="getImgPath(comic)" alt="">
+            <div class="card-body">
+            <h4 class="card-title">{{ comic.title }}</h4>
+            </div>
+        </div>
+      </a>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'ComicCard',
+    props: {
+        
+    },
+    data() {
+        return {
+            id: '2',
+            comic: null,
+            url_item: null,
+        }
+    },
+    methods: {
+        getImgPath(comic) {
+            return comic.thumbnail.path + '.' + comic.thumbnail.extension;
+        }
+    },
+    async mounted() {
+        this.url_item = '/Comic/' + this.id;
+
+        try {
+            const response = await axios.get('https://gateway.marvel.com:443/v1/public/comics/'+this.id+'?apikey=55dd7a6256658f33a00034a161f9c8f7&ts=1&hash=8e821d4269b2d7f35e61d11ecd39ff92');
+            this.comic = response.data.data.results[0];
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+</script>
+
+<style>
+.comic-item-card {
+    background-color: black;
+    width: 200px;
+}
+.comic-item-card .card-title {
+    color: white;
+}
+
+.comic-item-card img {
+    width: calc(100% - 10px);
+    margin: 5px;
+    align-content: center;
+}
+</style>
--- src/components/ComicsList.vue
+++ src/components/ComicsList.vue
@@ -1,3 +1,23 @@
 <template>
-    <h2>comics list</h2>
+    <div>
+        <h2>comics list</h2>
+        <ComicCard></ComicCard>
+    </div>
 </template>
+
+
+<script>
+import ComicCard from './ComicCard.vue';
+
+export default {
+  name: 'ComicsList',
+  components: {
+    // Demo,
+    ComicCard,
+  },
+}
+</script>
+
+<style>
+
+</style>
